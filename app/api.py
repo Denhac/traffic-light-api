86b00b68e420203900d0ac44db0095d4eeb3e2ca
Krav Lin <5847263+kravlin@users.noreply.github.com>
2019-03-26 21:56:44 -0600

    adding off link

--- app/api.py
+++ app/api.py
@@ -10,7 +10,7 @@ GPIO.setup(green_light, GPIO.OUT)
 GPIO.setup(yellow_light, GPIO.OUT)
 GPIO.setup(red_light, GPIO.OUT)
 
-@app.route('/color/green')
+@app.route('/green')
 def green():
     print("Turning on GREEN light")
     print("GREEN is on port {0}".format(green_light))
@@ -19,7 +19,7 @@ def green():
     GPIO.output(red_light, GPIO.LOW)
     return "Success"
 
-@app.route('/color/red')
+@app.route('/red')
 def red():
     print("Turning on RED light")
     print("RED is on port {0}".format(red_light))
@@ -28,10 +28,19 @@ def red():
     GPIO.output(red_light, GPIO.HIGH)
     return "Success"
 
-@app.route('/color/yellow')
+@app.route('/yellow')
 def yellow():
     print("Turning on YELLOW light")
     print("YELLOW is on port {0}".format(yellow_light))
     GPIO.output(green_light, GPIO.LOW)
     GPIO.output(yellow_light, GPIO.HIGH)
     GPIO.output(red_light, GPIO.LOW)
+    return "Success"
+
+@app.route('/off')
+def off():
+    print("Turning off lights")
+    GPIO.output(green_light, GPIO.LOW)
+    GPIO.output(yellow_light, GPIO.HIGH)
+    GPIO.output(red_light, GPIO.LOW)
+   
