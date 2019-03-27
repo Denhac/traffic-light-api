b6f2d3fea8cb474529f8b28030422f9f1a10dea1
Krav Lin <5847263+kravlin@users.noreply.github.com>
2019-03-26 21:32:18 -0600

    adding in more verbosity

--- app/api.py
+++ app/api.py
@@ -7,18 +7,24 @@ red_light = app.config["RED_LED"]
 
 @app.route('/color/green')
 def green():
+    print("Turning on GREEN light")
+    print("GREEN is on port {0}".format(green_light))
     GPIO.output(green_light, GPIO.HIGH)
     GPIO.output(yellow_light, GPIO.LOW)
     GPIO.output(red_light, GPIO.LOW)
 
 @app.route('/color/red')
 def red():
+    print("Turning on RED light")
+    print("RED is on port {0}".format(red_light))
     GPIO.output(green_light, GPIO.LOW)
     GPIO.output(yellow_light, GPIO.LOW)
     GPIO.output(red_light, GPIO.HIGH)
 
 @app.route('/color/yellow')
 def yellow():
+    print("Turning on YELLOW light")
+    print("YELLOW is on port {0}".format(yellow_light))
     GPIO.output(green_light, GPIO.LOW)
     GPIO.output(yellow_light, GPIO.HIGH)
     GPIO.output(red_light, GPIO.LOW)
