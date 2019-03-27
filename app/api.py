5048886719056443ecac09d822b452e3e8a59f13
Krav Lin <5847263+kravlin@users.noreply.github.com>
2019-03-26 21:51:58 -0600

    modifying to try to fix error

--- app/api.py
+++ app/api.py
@@ -1,11 +1,11 @@
 from app import app
 import RPi.GPIO as GPIO
 
-green_light = app.config["GREEN_LED"]
-yellow_light = app.config["YELLOW_LED"]
-red_light = app.config["RED_LED"]
+green_light = int(app.config["GREEN_LED"])
+yellow_light = int(app.config["YELLOW_LED"])
+red_light = int(app.config["RED_LED"])
 GPIO.setmode(GPIO.BCM)
-print(type(green_light))
+
 GPIO.setup(green_light, GPIO.OUT)
 GPIO.setup(yellow_light, GPIO.OUT)
 GPIO.setup(red_light, GPIO.OUT)
